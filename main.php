41f1fadf56c2e4b69cdf4c2b2342632537368496
HendrikdeJong <Hendrik.dejong06@gmail.com>
2023-11-23 14:40:13 +0100

    gwn dit ja

--- main.php
+++ main.php
@@ -1,2 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OurTube</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 <?php
 echo ('youtube video hier');
+?>
+<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank"></a>
+</body>
+</html>
