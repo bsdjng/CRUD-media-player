aee26ec201ae9363865f7722eade12b16dd96327
HendrikdeJong <Hendrik.dejong06@gmail.com>
2023-11-27 14:14:25 +0100

    Fix voor de Require
    je mag eigenlijk geen Head en Body in een Require gebruiken

--- Code/Header.php
+++ Code/Header.php
@@ -1,15 +1,10 @@
-<head>
-    <link rel="stylesheet" href="Header.css">
-</head>
-<body>
-    <ol class="Header">
-        <li class="HeaderItem"><a id="HeaderHomeLink" href="main.php"></a></li>
-        <li id="SearchBar">
-            <form action="Search.php" method="post">
-                <input id="SearchBarText"type="text" placeholder="Search">
-                <input id="SearchBarsubmit"type="submit" value="">
-            </form>
-        </li>
-        <li class="HeaderItem"><a id="HeaderUserLink" href="UserPage.php"></a></li>
-    </ol>
-</body>
+<ol class="Header">
+    <li class="HeaderItem"><a id="HeaderHomeLink" href="main.php"></a></li>
+    <li id="SearchBar">
+        <form action="Search.php" method="post">
+            <input id="SearchBarText" type="text" placeholder="Search">
+            <input id="SearchBarsubmit" type="submit" value="">
+        </form>
+    </li>
+    <li class="HeaderItem"><a id="HeaderUserLink" href="UserPage.php"></a></li>
+</ol>
