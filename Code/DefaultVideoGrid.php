b25ef19b9437b818f2fb46487b3f07718d25a45f
HendrikdeJong <Hendrik.dejong06@gmail.com>
2023-11-28 14:20:29 +0100

    push snel

---
+++ Code/DefaultVideoGrid.php
@@ -0,0 +1,70 @@
+<div class="MediaGrid">
+        <?php
+
+        $sqlVideos = "SELECT * FROM videos";
+        $videos = $pdo->query($sqlVideos);
+
+        $sqlAccounts = "SELECT * FROM accounts";
+        $accountsResult = $pdo->query($sqlAccounts);
+
+        // pleur het in een array
+        $accounts = $accountsResult->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($videos->rowCount() > 0) {
+            while ($video = $videos->fetch()) {
+                if (isset($video)) {
+        ?><ol class="GridItem">
+                            <?php
+                            // blob decoderen
+                            $imageSrc = "data:image/png;base64," . base64_encode($video['thumbnail_image']);
+                            // laat alle variabelen zien
+                            ?>
+                        <li class="thumbnail" style="background-image:url('<?php echo $imageSrc; ?>') ;">
+                            
+                        </li>
+                        <li>
+                            <?php
+                            echo $video['video_name'];
+                            ?>
+                        </li>
+                        <li>
+                            <?php // zoek de uploader van de video
+                            foreach ($accounts as $account) {
+                                if ($account['id'] == $video['account_id']) {
+                                    // blob decoderen
+                                    $imageSrc = "data:image/png;base64," . base64_encode($account['profile_picture']);
+                                    // laat alle variabelen zien
+                                    echo '<img class="PFP" src="' . $imageSrc . '" alt="Image">';
+                                    echo $account['username'];
+                                }
+                            }
+                            ?>
+                        </li>
+                        <li>
+                            <ol class="GridItem2">
+                                <li>
+                                    <?php
+                                    echo "views: " . $video['views'];
+                                    ?>
+                                </li>
+
+                                <li>
+                                    <?php
+                                    echo "Date Posted: " .  $video['created_at'];
+                                    ?>
+                                </li>
+                            </ol>
+                        </li>
+                    </ol>
+        <?php
+
+                } else {
+                    echo "No data found for this record.<br>";
+                }
+            }
+        } else {
+            echo "No results found.";
+        }
+        ?>
+
+    </div>
--- Code/main.php
+++ Code/main.php
@@ -7,6 +7,7 @@
     <title>OurTube</title>
     <link rel="stylesheet" href="Main.css">
     <link rel="stylesheet" href="Header.css">
+    <link rel="stylesheet" href="DefaultVideoGrid.css">
 </head>
 
 <body>
@@ -14,77 +15,12 @@
     require("Connection.php");
     require("Header.php");
     require("Search.php");
+    if(empty($_GET["search"]) == false){
+        var_dump($_GET["search"]);
+    }else {
+        require("DefaultVideoGrid.php");
+    }
     ?>
-    <div class="MediaGrid">
-        <?php
-
-        $sqlVideos = "SELECT * FROM videos";
-        $videos = $pdo->query($sqlVideos);
-
-        $sqlAccounts = "SELECT * FROM accounts";
-        $accountsResult = $pdo->query($sqlAccounts);
-
-        // pleur het in een array
-        $accounts = $accountsResult->fetchAll(PDO::FETCH_ASSOC);
-
-        if ($videos->rowCount() > 0) {
-            while ($video = $videos->fetch()) {
-                if (isset($video)) {
-        ?><ol class="GridItem">
-                        <li class="thumbnail">
-                            <?php
-                            // blob decoderen
-                            $imageSrc = "data:image/png;base64," . base64_encode($video['thumbnail_image']);
-                            // laat alle variabelen zien
-                            echo '<img src="' . $imageSrc . '" alt="Image">';
-                            ?>
-                        </li>
-                        <li>
-                            <?php
-                            echo $video['video_name'];
-                            ?>
-                        </li>
-                        <li>
-                            <?php // zoek de uploader van de video
-                            foreach ($accounts as $account) {
-                                if ($account['id'] == $video['account_id']) {
-                                    // blob decoderen
-                                    $imageSrc = "data:image/png;base64," . base64_encode($account['profile_picture']);
-                                    // laat alle variabelen zien
-                                    echo '<img class="PFP" src="' . $imageSrc . '" alt="Image">';
-                                    echo $account['username'];
-                                }
-                            }
-                            ?>
-                        </li>
-                        <li>
-                            <ol class="GridItem2">
-                                <li>
-                                    <?php
-                                    echo "views: " . $video['views'];
-                                    ?>
-                                </li>
-
-                                <li>
-                                    <?php
-                                    echo "Date Posted: " .  $video['created_at'];
-                                    ?>
-                                </li>
-                            </ol>
-                        </li>
-                    </ol>
-        <?php
-
-                } else {
-                    echo "No data found for this record.<br>";
-                }
-            }
-        } else {
-            echo "No results found.";
-        }
-        ?>
-
-    </div>
 </body>
 
 </html>
