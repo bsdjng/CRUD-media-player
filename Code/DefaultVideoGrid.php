b5a1a442e7c2662d25e10196532b77068b77143c
bsdjng <49759233+bsdjng@users.noreply.github.com>
2023-11-29 00:42:41 +0100

    list style none

--- Code/DefaultVideoGrid.php
+++ Code/DefaultVideoGrid.php
@@ -1,68 +1,68 @@
 <div class="MediaGrid">
-        <?php
+    <?php
 
-        $sqlVideos = "SELECT * FROM videos";
-        $videos = $pdo->query($sqlVideos);
+    $sqlVideos = "SELECT * FROM videos";
+    $videos = $pdo->query($sqlVideos);
 
-        $sqlAccounts = "SELECT * FROM accounts";
-        $accountsResult = $pdo->query($sqlAccounts);
+    $sqlAccounts = "SELECT * FROM accounts";
+    $accountsResult = $pdo->query($sqlAccounts);
 
-        // pleur het in een array
-        $accounts = $accountsResult->fetchAll(PDO::FETCH_ASSOC);
+    // pleur het in een array
+    $accounts = $accountsResult->fetchAll(PDO::FETCH_ASSOC);
 
-        if ($videos->rowCount() > 0) {
-            while ($video = $videos->fetch()) {
-                if (isset($video)) {
-        ?><ol class="GridItem">
-                            <?php
-                            // blob decoderen
-                            $imageSrc = "data:image/png;base64," . base64_encode($video['thumbnail_image']);
-                            // laat alle variabelen zien
-                            ?>
-                        <li class="thumbnail" style="background-image:url('<?php echo $imageSrc; ?>') ;"></li>
-                        <li>
-                            <?php
-                            echo $video['video_name'];
-                            ?>
-                        </li>
-                        <li>
-                            <?php // zoek de uploader van de video
-                            foreach ($accounts as $account) {
-                                if ($account['id'] == $video['account_id']) {
-                                    // blob decoderen
-                                    $imageSrc = "data:image/png;base64," . base64_encode($account['profile_picture']);
-                                    // laat alle variabelen zien
-                                    echo '<div class="PFP" style="background-image: url(\'' . $imageSrc . '\');"></div>';                                    // echo '<img class="PFP" src="' . $imageSrc . '" alt="Image">';
-                                    echo $account['username'];
-                                }
+    if ($videos->rowCount() > 0) {
+        while ($video = $videos->fetch()) {
+            if (isset($video)) {
+    ?><ol class="GridItem">
+                    <?php
+                    // blob decoderen
+                    $imageSrc = "data:image/png;base64," . base64_encode($video['thumbnail_image']);
+                    // laat alle variabelen zien
+                    ?>
+                    <li class="thumbnail" style="background-image:url('<?php echo $imageSrc; ?>') ;"></li>
+                    <li>
+                        <?php
+                        echo $video['video_name'];
+                        ?>
+                    </li>
+                    <li>
+                        <?php // zoek de uploader van de video
+                        foreach ($accounts as $account) {
+                            if ($account['id'] == $video['account_id']) {
+                                // blob decoderen
+                                $imageSrc = "data:image/png;base64," . base64_encode($account['profile_picture']);
+                                // laat alle variabelen zien
+                                echo '<div class="PFP" style="background-image: url(\'' . $imageSrc . '\');"></div>';                                    // echo '<img class="PFP" src="' . $imageSrc . '" alt="Image">';
+                                echo $account['username'];
                             }
-                            ?>
-                        </li>
-                        <li>
-                            <ol class="GridItem2">
-                                <li>
-                                    <?php
-                                    echo "views: " . $video['views'];
-                                    ?>
-                                </li>
+                        }
+                        ?>
+                    </li>
+                    <li>
+                        <ol class="GridItem2">
+                            <li>
+                                <?php
+                                echo "views: " . $video['views'];
+                                ?>
+                            </li>
 
-                                <li>
-                                    <?php
-                                    echo "Date Posted: " .  $video['created_at'];
-                                    ?>
-                                </li>
-                            </ol>
-                        </li>
-                    </ol>
-        <?php
+                            <li>
+                                <?php
+                                echo "Date Posted: " .  $video['created_at'];
+                                ?>
+                            </li>
+                        </ol>
+                    </li>
+                </ol>
+    <?php
 
-                } else {
-                    echo "No data found for this record.<br>";
-                }
+            } else {
+                echo "No data found for this record.<br>";
             }
-        } else {
-            echo "No results found.";
         }
-        ?>
+    } else {
+        echo "No results found.";
+    }
+    ?>
 
-    </div>
+</div>
