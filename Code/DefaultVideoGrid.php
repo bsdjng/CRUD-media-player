51fbfe8aef7355409867f5a55e8c73632f3714bf
bsdjng <49759233+bsdjng@users.noreply.github.com>
2023-11-29 09:57:02 +0100

    Update DefaultVideoGrid.php
    tijd berekenen

--- Code/DefaultVideoGrid.php
+++ Code/DefaultVideoGrid.php
@@ -20,7 +20,7 @@
                     // laat alle variabelen zien
                     ?>
                     <li class="thumbnail" style="background-image:url('<?php echo $imageSrc; ?>') ;"></li>
-                    <li class="VidName" >
+                    <li class="VidName">
                         <?php
                         echo $video['video_name'];
                         ?>
@@ -28,7 +28,7 @@
                     <li>
                         <ol class="GridItem2">
                             <li>
-                            <?php // zoek de uploader van de video
+                                <?php // zoek de uploader van de video
                                 foreach ($accounts as $account) {
                                     if ($account['id'] == $video['account_id']) {
                                         // blob decoderen
@@ -38,27 +38,47 @@
                                         echo $account['username'];
                                     }
                                 }
-                            ?>
+                                ?>
                             </li>
                             <li>
-                                <li>
-                                    <ol class="GridItem3">
-                                        <li>
-                                            <?php
-                                            echo "views: " . $video['views'];
-                                            ?>
-                                        </li>
+                            <li>
+                                <ol class="GridItem3">
+                                    <li>
+                                        <?php
+                                        echo "views: " . $video['views'];
+                                        ?>
+                                    </li>
 
-                                        <li>
-                                            <?php
-                                            echo "Date Posted: " .  $video['created_at'];
-                                            ?>
-                                        </li>
-                                    </ol>
-                                </li>
+                                    <li>
+                                        <?php
+                                        $createdDateTime = new DateTime($video['created_at']);
+                                        // huidige tijd
+                                        $currentDateTime = new DateTime();
+                                        // Tijdverschil berekenen
+                                        $timeDifference = $currentDateTime->diff($createdDateTime);
+                                        // Display the time difference
+                                        if ($timeDifference->days > 0) {
+                                            if ($timeDifference->days == 1) {
+                                                echo $timeDifference->days . ' day ago';
+                                            } else {
+                                                echo $timeDifference->days . ' days ago';
+                                            }
+                                        } elseif ($timeDifference->h > 0) {
+                                            if ($timeDifference->h == 1) {
+                                                echo $timeDifference->h . ' hours ago';
+                                            } else {
+                                                echo $timeDifference->h . ' hours ago';
+                                            }
+                                        } else {
+                                            echo $timeDifference->i . ' minutes ago';
+                                        }
+                                        ?>
+                                    </li>
+                                </ol>
                             </li>
-                        </ol>
                     </li>
+                </ol>
+                </li>
                 </ol>
     <?php
 
