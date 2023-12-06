fb936b8271a1bbf6178dacbd3f4bb49028df1685
HendrikdeJong <Hendrik.dejong06@gmail.com>
2023-12-06 10:00:25 +0100

    Update processing.php

--- Code/processing.php
+++ Code/processing.php
@@ -17,10 +17,24 @@ if (!empty($username) && !empty($email) && !empty($password) && !empty($repeat_p
 
         $date_Now = (new DateTime())->format('Y-m-d H:i:s');
 
+        // Assuming $profile_pictureSrc is the file path on the server
+        $profile_pictureSrc = '../images/user.png';
+
+        // Read the file content
+        $profile_picture_content = file_get_contents($profile_pictureSrc);
+
         // Insert data into the 'Accounts' table
-        $sql = "INSERT INTO Accounts (username, email, password, created_at) VALUES (:username, :email, :hashed_password, :date_Now)";
+        $sql = "INSERT INTO Accounts (username, email, password, created_at, profile_picture) VALUES (:username, :email, :hashed_password, :date_Now, :profile_picture)";
         $stmt = $pdo->prepare($sql);
-        $stmt->execute([$username, $email, $hashed_password, $date_Now]);
+
+        // Bind parameters
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':hashed_password', $hashed_password);
+        $stmt->bindParam(':date_Now', $date_Now);
+        $stmt->bindParam(':profile_picture', $profile_picture_content, PDO::PARAM_LOB);
+
+        $stmt->execute();
 
         header('Location: Main.php');
         exit();
@@ -28,3 +42,4 @@ if (!empty($username) && !empty($email) && !empty($password) && !empty($repeat_p
 } else {
     echo "All fields are required.";
 }
+?>
